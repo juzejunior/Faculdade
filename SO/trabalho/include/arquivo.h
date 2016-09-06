0a07ee90f1c49c45ae753124744f84d56d64f486
Wellington Correia <wellington.correia02@gmail.com>
2016-09-06 00:37:25 -0300

    Função de leitura, acho que esta funcionando

--- SO/trabalho/include/arquivo.h
+++ SO/trabalho/include/arquivo.h
@@ -4,8 +4,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char leitura(int *linha, int *coluna){
+void domencao(int *linhas, int *colunas){
 	FILE *arquivo;
-	arquivo = fopen(diagrama.txt,"r");
+	int cont,x=0,y=0;
+	arquivo = fopen("diagrama.txt","r");
+	fscanf(arquivo,"%d %d",linhas,colunas);
+	fclose(arquivo);
+}
+
+
+void leitura(char vet[][]){
+	char ch;
+	FILE *arquivo;
+	int cont,x=0,y=0,linhas,colunas;
+	arquivo = fopen("diagrama.txt","r");
+	fscanf(arquivo,"%d %d",&linhas,&colunas);
+	//char vet[linhas+2][colunas];
+	//printf("%d %d", linhas, colunas);
+	while((	ch = fgetc(arquivo)) != EOF ){
+		if(ch != '\n'){
+			vet[x][y] = ch;
+				y++;
+		}else{ 
+			x++;
+			y = 0;
+		}if(x == linhas+3){
+			break;
+		}
+	}	
+	fclose(arquivo);	
 	
 }
+
+#endif
