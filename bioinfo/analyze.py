ec603de915a374fbd5a92cb1e8c2633183d10659
Mateus Nascimento <mateus.nnascimento.s@gmail.com>
2018-04-09 17:24:00 -0300

    Adicionado função de rna

--- bioinfo/analyze.py
+++ bioinfo/analyze.py
@@ -5,8 +5,9 @@
 #
 
 CODON_SIZE = 3
-start_codons = ['atg']
-amino_acids = ["M"("abc", "cba", "cblow")]
+start_codons = 'atg'
+end_codons = 'ccc'
+amino_acids = ["M", ("abc", "cba", "cblow")]
 
 ''' Objective: Receive a interval and get string between it from a file
     Params: initial_position(initial carater position), final_position(final caracter position)
@@ -22,26 +23,85 @@ def get_sequence(initial_position, final_position):
 def invert_sequence(sequence):
     return ''.join(reversed(sequence))
 
-# TODO Change each nucleotide to their reverse, receives a string and return a string
+def get_rna(sequence):
+    result = ""
+    for nucleotideo in sequence:
+        if (nucleotideo == 'a'):
+            result += "u"
+        elif (nucleotideo == "t"):
+            result += "a"
+        elif (nucleotideo == "g"):
+            result += "c"
+        elif (nucleotideo == "c"):
+            result += "g"
+    return result
+
+def get_reverse_nucleotide(sequence):
+    reversed = invert_sequence(sequence)
+    result = ""
+    for nucleotideo in reversed:
+        if (nucleotideo == 'a'):
+            result += "t"
+        elif (nucleotideo == "t"):
+            result += "a"
+        elif (nucleotideo == "g"):
+            result += "c"
+        elif (nucleotideo == "c"):
+            result += "g"
+    return result
 
 def get_receipes(sequence):
-    string_test = "ggagccatgccctttttgtaaatc"
-    sequence_list = list(string_test)
+    #string_test = "ggagccatgccctttttgtaaatgc"
+    #sequence_list = list(string_test)
     codon = ""
-    for nucleotide in sequence_list:
-        if len(codon) == CODON_SIZE:
-            if codon in start_codons:
-                print(codon)
-            codon = ""
-            codon += nucleotide
-        else:
-            codon += nucleotide
+    previous = ""
+    position = 0
+    pos_ini = 0
+    pos_fim = 0
+    start = 0
+    end = 0
+    for nucleotide in sequence:
+        position+=1
+        if (start == 0):
+            if (nucleotide == start_codons[pos_ini]):
+                codon += nucleotide
+                pos_ini+=1
+                if (pos_ini==3):
+                    start = 1
+            else:
+                codon = ""
+                pos_ini = 0
+        elif (start == 1):
+            if (nucleotide == end_codons[pos_fim]):
+                previous += nucleotide
+                pos_fim += 1
+                if (pos_fim == 3):
+                    codon += end_codons
+                    start = 2
+            elif (previous != ""):
+                pos_fim = 0
+                if (nucleotide == end_codons[pos_fim]):
+                    pos_fim += 1
+                else:
+                    codon += previous
+                    codon += nucleotide
+                    previous = ""
+            else:
+                codon += nucleotide
+                pos_fim = 0
+                if (nucleotide == end_codons[pos_fim]):
+                    pos_fim += 1
+                else:
+                    previous = ""
+        elif (start == 2): break
+
     return codon
 
 # TODO print each amino acids here, receives a list of list and print each rna and your respective amino acids
 
+
 if __name__ == '__main__':
-    print(amino_acids)
-    #sequence = get_sequence(3462111, 3463457).lower()
-    #codon_one = get_receipes(invert_sequence(sequence))
+    sequence = get_sequence(3462111, 3463457).lower()
+    # codon_one = get_receipes(invert_sequence(sequence))
+    print(get_receipes(sequence))
     # transform each from rna to nucleotide
